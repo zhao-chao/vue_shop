6c16833fea36b56658ae53065c7dc3b89f9f7e8a
/配置姓名 <1299898126@qq.com>
2020-11-28 09:42:17 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="app">
-  
-  </div>
+    <div id="app">
+  <!-- 路由占位符 -->
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
---
+++ src/components/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="login_container">
+    <!-- 登录盒子  -->
+    <div class="login_box">
+      <!-- 头像 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- 登录表单 -->
+      <el-form
+        :model="loginForm"
+        ref="LoginFormRef"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped> 
+    .login_container{
+        background-color: #2b4b6b;
+        height: 100%;
+    }
+    .login_box{
+        width: 450px;
+        height: 300px;
+        background-color: #fff;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+    }
+</style>
