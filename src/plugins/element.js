8f3b1b96ea838efe8c0033299c4f408b37904d90
/配置姓名 <1299898126@qq.com>
2020-11-28 18:17:17 +0800

    3

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,11 +5,27 @@ import Vue from 'vue'
 // // 导入弹框提示组件
 // import { Message } from 'element-ui'
 
-import { Message, Button, Form, FormItem, Input } from 'element-ui'
+import {
+	Message,
+	Button,
+	Form,
+	FormItem,
+	Input,
+	Container,
+	Header,
+	Aside,
+	Main,
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+
+// 主页的组件
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 // 弹出框组件 需要全局挂载
 Vue.prototype.$message = Message
